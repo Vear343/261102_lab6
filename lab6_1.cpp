4ebff0d544ba93197c06c82e49bae554e0f29452
Vear <arnuwat.vear@gmail.com>
2025-12-09 16:37:38 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,34 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    int i = 0;
+    int number[999];
+
+    while (true){
+        cout << "Enter an integer: ";
+        cin >> number[i];
+        if (number[i] != 0){
+            i++;
+        }
+        else{
+            break;
+        }
+    }
+
+    int even = 0;
+    int odd = 0;
+
+    for (int j = 0; j < i;j++){
+        if (number[j] % 2 == 0){
+            even++;
+        }
+        else{
+            odd++;
+        }
+    }
+
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
     return 0;
 }
